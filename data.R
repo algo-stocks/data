df3d0ce6c71131843220a610934200e7237199f4
Nguyen Dac Doan <nguyendacdoan@gmail.com>
2022-03-18 10:24:01 +0700

    R index

--- data.R
+++ data.R
@@ -4,11 +4,23 @@ library(dplyr)
 
 if (!file.exists('data.py')) download.file('https://raw.githubusercontent.com/algo-stocks/data/master/data.py', 'data.py')
 
-data = import('data')
+pdata = import('data')
+
+
+data = {}
+rdata = NULL
+
+data$set_source <- function(src) {
+  if (src != pdata$source) {
+    pdata$set_source(src)
+    rdata <<- NULL
+  }
+}
 
 loadPrice <- function(...) {
-  if (is.null(data$data)) {
-    data$load_data()
+  if (is.null(rdata)) {
+    rdata <<- pdata$load_data()
   }
-  data$data %>% filter(ticker %in% c(...))
+  tickers = toupper(c(...))
+  rdata %>% filter(ticker %in% c(paste('^', tickers, sep = ''), tickers))
 }
